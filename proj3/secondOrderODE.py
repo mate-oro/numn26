858f261e88ad0978f09f8a65b316b03731ac66f7
Mate Oro <83836110+mate-oro@users.noreply.github.com>
2025-03-25 12:14:25 +0100

    The updated second order solver

--- proj3/secondOrderODE.py
+++ proj3/secondOrderODE.py
@@ -50,7 +50,7 @@ class Second_Order(Explicit_ODE):
         # Solver options
         self.options["h"] = 0.01
         self.options["damping"] = problem.damping
-        self.options["maxsteps"] = int(50000)
+        self.options["maxsteps"] = int(500000)
         
         # Statistics
         self.statistics["nsteps"] = 0
@@ -107,7 +107,7 @@ class Newmark(Second_Order):
         """
         integrates (t,y) values until t > tf
         """
-        h = self.options["h"]
+        h = self.h
         h = min(h, abs(tf - t))
         y = np.atleast_1d(y)
         f = self.problem.f
@@ -136,6 +136,7 @@ class Newmark(Second_Order):
             tres.append(t_np1)
 
             t, p_n, v_n, a_n = t_np1, p_np1, v_np1, a_np1
+            h = min(self.h, np.abs(tf - t))
         else:
             raise Explicit_ODE_Exception(f'Final time not reached within maximum number of steps at t={t}')
 
@@ -146,19 +147,18 @@ class Newmark(Second_Order):
 
         f = self.problem.f
         ga = self.gamma
+        be = self.beta
         t_np1 = t_n + h
         
-        if self.damping is False and self.beta == 0:
+        if self.damping is False and be == 0:
             self.statistics["nfcns"] += 1
             p_np1 = p_n + h * v_n + 0.5 * h ** 2 * a_n
             a_np1 = f(t_np1, p_np1, v_n)
             v_np1 = v_n + h * ((1 - ga) * a_n + ga * a_np1)
             return t_np1, p_np1, v_np1, a_np1
         else:
-            be = self.beta
-
             # Set up equations for v_np1 and a_np1 in terms of p_np1
-            eq_a = lambda p_np1: (1 - 1 / (2 * be)) * a_n - (1 / (be * h)) * v_n  + (1 / (2 * be * h ** 2)) * (p_np1 - p_n)
+            eq_a = lambda p_np1: (1 - 1 / (2 * be)) * a_n - (1 / (be * h)) * v_n  + (1 / (be * h ** 2)) * (p_np1 - p_n)
             eq_v = lambda p_np1: v_n + h * ((1 - ga) * a_n + ga * eq_a(p_np1))
 
             # Now set up the equation that can be solved for p_np1 and then solve it
@@ -255,6 +255,7 @@ class HHTalpha(Second_Order):
             tres.append(t_np1)
 
             t, p_n, v_n, a_n = t_np1, p_np1, v_np1, a_np1
+            h = min(self.h, np.abs(tf - t))
         else:
             raise Explicit_ODE_Exception(f'Final time not reached within maximum number of steps at t={t}')
 
@@ -271,12 +272,12 @@ class HHTalpha(Second_Order):
         ga = self.gamma
 
         # Set up equations for v_np1 and a_np1 in terms of p_np1
-        eq_a = lambda p_np1: (1 - 1 / (2 * be)) * a_n - (1 / (be * h)) * v_n  + (1 / (2 * be * h ** 2)) * (p_np1 - p_n)
+        eq_a = lambda p_np1: (1 - 1 / (2 * be)) * a_n - (1 / (be * h)) * v_n  + (1 / (be * h ** 2)) * (p_np1 - p_n)
         eq_v = lambda p_np1: v_n + h * ((1 - ga) * a_n + ga * eq_a(p_np1))
 
         # Now set up the equation that can be solved for p_np1 and then solve it
         p_guess = p_n + h * v_n + 0.5 * h ** 2 * a_n
-        p_np1, infodict, ier, _  = fsolve(lambda x: eq_a(x) - (1 + al) * f(t_np1, x, eq_v(x)) + al * a_n, p_guess, full_output=True)
+        p_np1, infodict, ier, _  = fsolve(lambda x: eq_a(x) - (1 + al) * f(t_np1, x, eq_v(x)) + al * f(t_n, p_n, v_n), p_guess, full_output=True)
         self.statistics["nfcns"] += infodict["nfev"]
         
         if ier != 1:
@@ -477,6 +478,7 @@ class Newmark_lin(Second_Order_lin):
             tres.append(t_np1)
 
             t, p_n, v_n, a_n, f_n = t_np1, p_np1, v_np1, a_np1, f_np1
+            h = min(self.h, np.abs(tf - t))
         else:
             raise Explicit_ODE_Exception('Final time not reached within maximum number of steps')
 
@@ -509,7 +511,7 @@ class Newmark_lin(Second_Order_lin):
                 + C @ ((ga / (be * h)) * p_n - (1 - ga / be) * v_n - (1 - ga / (2 * be)) * h * a_n)
             
             # Set up equations for a_np1 and v_np1 in terms of the previously calculated terms
-            eq_a = lambda p_np1: (1 - 1 / (2 * be)) * a_n - (1 / (be * h)) * v_n  + (1 / (2 * be * h ** 2)) * (p_np1 - p_n)
+            eq_a = lambda p_np1: (1 - 1 / (2 * be)) * a_n - (1 / (be * h)) * v_n  + (1 / (be * h ** 2)) * (p_np1 - p_n)
             eq_v = lambda a_np1: v_n + h * ((1 - ga) * a_n + ga * a_np1)
             
             # Solve the system
@@ -531,3 +533,127 @@ class Newmark_lin(Second_Order_lin):
         self.log_message(f' Damping           : {self.damping}',                 verbose)
         self.log_message(f' beta:             : {self.beta}',                    verbose)
         self.log_message(f' gamma:            : {self.gamma}\n' ,                  verbose)
+
+class HHTalpha_lin(Second_Order_lin):
+    def __init__(self, problem):
+        super().__init__(problem)
+
+        self.options["alpha"] = 0
+        self.options["beta"] = 0.25
+        self.options["gamma"] = 0.5
+
+    # Setting up the method parameters
+    @property
+    def alpha(self) -> float:
+        return self.options["alpha"]
+    @alpha.setter
+    def alpha(self, alpha: float):
+            if alpha < -1/3 or alpha > 0:
+                 raise ValueError("\"alpha\" must be in the range [-1/3, 0]")
+            self.options["alpha"] = alpha
+            self.options["beta"] = ((1 - alpha) / 2) ** 2
+            self.options["gamma"] = 1 / 2 - alpha
+
+    @property
+    def beta(self) -> float:
+        return self.options["beta"]
+    @beta.setter
+    def beta(self, beta: float):
+        warn("\"beta\" should not be set manually. It is fully determined by \"alpha\"")
+    
+    @property
+    def gamma(self) -> float:
+        return self.options["gamma"]
+    @gamma.setter
+    def gamma(self, gamma: float):
+        warn("\"gamma\" should not be set manually. It is fully determined by \"alpha\"")
+    
+    def integrate(self, t: float, y: np.ndarray, tf: float, opts) -> tuple[ID_PY_OK, list[float], list[np.ndarray]]:
+        """
+        integrates (t,y) values until t > tf
+        """
+        h = self.options["h"]
+        h = min(h, abs(tf - t))
+        y = np.atleast_1d(y)
+
+        # Separating components
+        p_n = y[:self.problem.ndofs]
+        v_n = y[self.problem.ndofs:]
+
+        # Computing initial a
+        M = self.problem.Mass_mat
+        K = self.problem.Stiff_mat
+        F = self.problem.force
+        f_n = F(t)
+        if self.damping is False and self.beta == 0:
+            a_n = ssl.spsolve(M, f_n - K @ p_n)
+        else:
+            C = self.problem.Damp_mat
+            a_n = ssl.spsolve(M, F(t) - C @ v_n -  K @ p_n)
+        a_n = np.array(a_n)
+        self.statistics["nfcns"] += 1
+
+        #Lists for storing the result
+        tres = []
+        yres = []
+
+        for i in range(self.maxsteps):
+            if t >= tf:
+                break
+            self.statistics["nsteps"] += 1
+
+            t_np1, p_np1, v_np1, a_np1, f_np1 = self.HHTalphaStep(t, p_n, v_n, a_n, h, f_n)
+
+            y_np1 = np.hstack((p_np1, v_np1))
+            yres.append(y_np1)
+            tres.append(t_np1)
+
+            t, p_n, v_n, a_n, f_n = t_np1, p_np1, v_np1, a_np1, f_np1
+            h = min(self.h, np.abs(tf - t))
+        else:
+            raise Explicit_ODE_Exception('Final time not reached within maximum number of steps')
+
+        return ID_PY_OK, tres, yres
+    
+    def HHTalphaStep(self, t_n: float, p_n: np.ndarray, v_n: np.ndarray, a_n: np.ndarray, h:float, f_n: np.ndarray)\
+                    -> tuple[float, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+
+        M = self.problem.Mass_mat
+        K = self.problem.Stiff_mat
+        F = self.problem.force
+        C = self.problem.Damp_mat
+        al = self.alpha
+        be = self.beta
+        ga = self.gamma
+        t_np1 = t_n + h
+        f_np1 = F(t_np1)
+
+        # Set up the linear equation to be solved for p_np1
+        A = (1 / (be * h ** 2)) * M + (ga / (be * h)) * C + (1 + al) * K
+        b = f_n\
+            + M @ ((1 / (be * h ** 2)) * p_n + (1 / (be * h)) * v_n + (1 / (2 * be) - 1) * a_n)\
+            + C @ ((ga / (be * h)) * p_n - (1 - ga / be) * v_n - (1 - ga / (2 * be)) * h * a_n)\
+            + al * K @ p_n
+        
+        # Set up equations for a_np1 and v_np1 in terms of the previously calculated terms
+        eq_a = lambda p_np1: (1 - 1 / (2 * be)) * a_n - (1 / (be * h)) * v_n  + (1 / (be * h ** 2)) * (p_np1 - p_n)
+        eq_v = lambda a_np1: v_n + h * ((1 - ga) * a_n + ga * a_np1)
+        
+        # Solve the system
+        self.statistics["nfcns"] += 1
+        p_np1 = np.array(ssl.spsolve(A, b))
+        a_np1 = eq_a(p_np1)
+        v_np1 = eq_v(a_np1)
+
+        return t_np1, p_np1, v_np1, a_np1, f_np1
+    
+    def print_statistics(self, verbose=NORMAL): # type: ignore
+        self.log_message('\nFinal Run Statistics             : {name}'.format(name=self.problem.name),        verbose)
+        self.log_message(' Step-length                     : {stepsize} '.format(stepsize=self.options["h"]), verbose)
+        self.log_message(' Number of Steps                 : '+str(self.statistics["nsteps"]),          verbose)               
+        self.log_message(' Number of Function Evaluations  : '+str(self.statistics["nfcns"]),           verbose)
+
+        self.log_message('\nSolver options:',                                  verbose)
+        self.log_message(' Solver            : HHT-alpha',                        verbose)
+        self.log_message(f' Damping           : {self.damping}',                 verbose)
+        self.log_message(f' alpha:            : {self.alpha}',                    verbose)
